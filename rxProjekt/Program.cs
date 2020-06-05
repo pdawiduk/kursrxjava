5946e6398550f470864a964a037fa701024ba15c
Monika Strzelec <monika.strzelec1@unilodz.eu>
2020-06-05 15:48:23 +0200

    Dodaj pliki projektów.

---
+++ rxProjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rxProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
